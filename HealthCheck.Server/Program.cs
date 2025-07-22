cf360552f3c164be395895619ceb4c038a1f0f51
Akmal Fazili <92304771+Akmalfazili@users.noreply.github.com>
2025-07-22 14:56:14 +0800

    test service worker and added CORS setting

--- HealthCheck.Server/Program.cs
+++ HealthCheck.Server/Program.cs
@@ -1,4 +1,5 @@
 global using HealthCheck.Server;
+using Microsoft.AspNetCore.Cors;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+options.AddPolicy(name: "AngularPolicy",
+cfg =>
+{
+    cfg.AllowAnyHeader();
+    cfg.AllowAnyMethod();
+    cfg.WithOrigins(builder.Configuration["AllowedCORS"]);
+}));
 
 var app = builder.Build();
 
@@ -26,6 +36,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseCors("AngularPolicy");
 app.UseHealthChecks(new PathString("/api/health"),
     new CustomerHealthCheckOptions());
 app.MapControllers();
